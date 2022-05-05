4468c4b0db503e5c12020c59e59f7e0228250207
Kjasuquo <joseph.asuquo@decagon.dev>
2022-05-05 10:40:52 +0100

    clean up done

--- database/db_interface.go
+++ database/db_interface.go
@@ -12,17 +12,6 @@ import (
 // DB provides access to the different db
 //go:generate  mockgen -source=./db_interface.go -destination=./mockfile.go DB -package=mock
 type DB interface {
-	//CreateSeller(user *models.Seller) (*models.Seller, error)
-	//CreateBuyer(user *models.Buyer) (*models.Buyer, error)
-	//FindSellerByUsername(username string) (*models.Seller, error)
-	//FindBuyerByUsername(username string) (*models.Buyer, error)
-	//FindSellerByEmail(email string) (*models.Seller, error)
-	//FindBuyerByEmail(email string) (*models.Buyer, error)
-	//FindSellerByPhone(phone string) (*models.Seller, error)
-	//FindBuyerByPhone(phone string) (*models.Buyer, error)
-	//FindAllSellersExcept(except string) ([]models.Seller, error)
-	//UpdateUser(user *models.User) error
-	//TokenInBlacklist(token *string) bool
 	SearchDB(c *gin.Context) ([]models.Product, error)
 }
 
