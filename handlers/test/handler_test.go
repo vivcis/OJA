6b3d3a7f69b464d5503454e5cc52b010f8650d9d
majorchork <majorchork@gmail.com>
2022-05-11 11:56:48 +0100

    update

--- handlers/test/handler_test.go
+++ handlers/test/handler_test.go
@@ -1,1 +1,67 @@
 package test
+
+import (
+	"encoding/json"
+	mock_database "github.com/decadevs/shoparena/database/mocks"
+	"github.com/decadevs/shoparena/handlers"
+	"github.com/decadevs/shoparena/models"
+	"github.com/decadevs/shoparena/router"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/crypto/bcrypt"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestResetPassword(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockDB := mock_database.NewMockDB(ctrl)
+	mockMail := mock_database.NewMockMailer(ctrl)
+	h := &handlers.Handler{DB: mockDB, Mail: mockMail}
+
+	route, _ := router.SetupRouter(h)
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte("12345678"), bcrypt.DefaultCost)
+
+	resetPassword := struct {
+		OldPassword        string `json:"old_password"`
+		NewPassword        string `json:"new_password"`
+		ConfirmNewPassword string `json:"confirm_new_password"`
+	}{
+		OldPassword:        "12345678",
+		NewPassword:        "12345678",
+		ConfirmNewPassword: "12345678",
+	}
+
+	buyer := models.Buyer{
+		User: models.User{
+			Email:        "chuks@gmail.com",
+			PasswordHash: string(passwordHash),
+		},
+		Cart:   nil,
+		Orders: nil,
+	}
+
+	mockDB.EXPECT().FindBuyerByEmail("chuks@gmail.com").Return(&buyer, nil)
+	mockDB.EXPECT().BuyerUpdatePassword(string(passwordHash), gomock.Any()).Return(&buyer, nil)
+
+	resetPasswordPayload, err := json.Marshal(resetPassword)
+	if err != nil {
+		log.Println(err)
+		t.Fail()
+	}
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("PUT", "/api/v1/buyer/resetpassword/chuks@gmail.com",
+		strings.NewReader(string(resetPasswordPayload)))
+
+	route.ServeHTTP(w, req)
+	assert.Contains(t, w.Body.String(), "reset", "password")
+	assert.Equal(t, w.Code, http.StatusOK)
+}
+
+func TestResetPassword_Wrong_Password(t *testing.T) {
+
+}
