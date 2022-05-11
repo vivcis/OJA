e5d5c0d588449de09f2d5ba1857f1c50209f594c
majorchork <majorchork@gmail.com>
2022-05-11 11:55:57 +0100

    update

--- models/shop.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Shop struct {
-	ID       uint   `gorm:"primarykey"`
-	ShopName string `json:"shop_name"`
-	SellerID uint   `json:"seller_id"`
-	Seller   Seller
-	Product  []Product `gorm:"many2many:shop_products;"`
-}
