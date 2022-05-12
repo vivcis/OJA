e484e0379700041b1136772f37c62e62d5f896aa
dilly3 <michael.anikamadu@gmail.com>
2022-05-12 18:47:07 +0100

    jwtauth

--- services/jwtauth.go
+++ services/jwtauth.go
@@ -1,1 +1,32 @@
 package services
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"os"
+	"time"
+)
+
+func GenerateTokenWithMethod(signMethod *jwt.SigningMethodHMAC, claims jwt.MapClaims, secret *string) (*string, error) {
+	// Create a new token object, specifying signing method and the claims
+	// you would like it to contain.
+	token := jwt.NewWithClaims(signMethod, claims)
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := token.SignedString([]byte(*secret))
+	if err != nil {
+		return nil, err
+	}
+	return &tokenString, nil
+}
+
+func GenerateTokenWithClaims(username string) string {
+	claims := jwt.MapClaims{
+		"exp":      time.Now().Add(time.Hour * 3).Unix(),
+		"iat":      time.Now().Unix(),
+		"username": username,
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	t, _ := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	return t
+
+}
