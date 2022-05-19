743b152b7d0c3a92cfd7718a901a49ed99626a34
vivcis <ceciliaorji.co@gmail.com>
2022-05-19 22:04:36 +0100

    upload buyer image tested

--- handlers/test/uploadBuyerPic_test.go
+++ handlers/test/uploadBuyerPic_test.go
@@ -1,7 +1,6 @@
 package test
 
 import (
-	"bytes"
 	"fmt"
 	mock_database "github.com/decadevs/shoparena/database/mocks"
 	"github.com/decadevs/shoparena/handlers"
@@ -12,58 +11,13 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"gorm.io/gorm"
-	"image"
-	"image/color"
-	"image/png"
-	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
 	"os"
 	"testing"
 )
 
-func createImg() (*os.File, error) {
-	width := 200
-	height := 100
-	upLeft := image.Point{0, 0}
-	lowRight := image.Point{width, height}
-	img := image.NewRGBA(image.Rectangle{upLeft, lowRight})
-	// Colors are defined by Red, Green, Blue, Alpha uint8 values.
-	cyan := color.RGBA{100, 200, 200, 0xff}
-	// Set color for each pixel.
-	for x := 0; x < width; x++ {
-		for y := 0; y < height; y++ {
-			switch {
-			case x < width/2 && y < height/2: // upper left quadrant
-				img.Set(x, y, cyan)
-			case x >= width/2 && y >= height/2: // lower right quadrant
-				img.Set(x, y, color.White)
-			default:
-				// Use zero value.
-			}
-		}
-	}
-	// Encode as PNG.
-	f, err := os.Create("testimg.png")
-	if err != nil {
-		return nil, err
-	}
-	png.Encode(f, img)
-	return f, nil
-}
-func prepfile(file *os.File) (*bytes.Buffer, string, error) {
-	var b bytes.Buffer
-	w := multipart.NewWriter(&b)
-	defer w.Close()
-	fmt.Println(file.Name())
-	if _, err := w.CreateFormFile("profile_picture", file.Name()); err != nil {
-		return nil, "", fmt.Errorf("%v", err)
-	}
-	fmt.Println(w.FormDataContentType())
-	return &b, w.FormDataContentType(), nil
-}
-
-func TestUploadSellerProfielPic(t *testing.T) {
+func TestUploadBuyerProfielPic(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 	mockDB := mock_database.NewMockDB(ctrl)
@@ -78,7 +32,7 @@ func TestUploadSellerProfielPic(t *testing.T) {
 		t.Fail()
 	}
 	file, _ := createImg()
-	seller := models.Seller{
+	buyer := models.Buyer{
 		User: models.User{
 			Model: gorm.Model{
 				ID: 23,
@@ -94,7 +48,7 @@ func TestUploadSellerProfielPic(t *testing.T) {
 		Orders: nil,
 	}
 
-	mockDB.EXPECT().FindSellerByEmail(seller.Email).Return(&seller, nil).Times(3).AnyTimes()
+	mockDB.EXPECT().FindBuyerByEmail(buyer.Email).Return(&buyer, nil).Times(3).AnyTimes()
 	mockDB.EXPECT().TokenInBlacklist(gomock.Any()).Return(false).Times(3).AnyTimes()
 	mockDB.EXPECT().UploadFileToS3(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).Return("", nil).AnyTimes()
 
@@ -104,7 +58,7 @@ func TestUploadSellerProfielPic(t *testing.T) {
 			fmt.Errorf("%v", err)
 		}
 		resp := httptest.NewRecorder()
-		req, _ := http.NewRequest(http.MethodPut, "/api/v1/uploadsellerpic", b)
+		req, _ := http.NewRequest(http.MethodPut, "/api/v1/uploadbuyerpic", b)
 		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", *accToken))
 		req.Header.Set("Content-Type", content_type)
 		route.ServeHTTP(resp, req)
