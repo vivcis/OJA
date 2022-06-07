6cd1702bd345cc4ec84d69dc55bfbc380246c1bc
Michael <michaelgbenle@gmail.com>
2022-06-07 10:12:38 +0100

    buyer order route adjusted

--- router/router.go
+++ router/router.go
@@ -63,14 +63,14 @@ func SetupRouter(h *handlers.Handler) (*gin.Engine, string) {
 		authorizedRoutesBuyer.DELETE("/deletefromcart/:id", h.DeleteFromCart)
 		authorizedRoutesBuyer.DELETE("/deleteallcart", h.DeleteAllCartProducts)
 		authorizedRoutesBuyer.POST("/buyer/logout", h.HandleLogoutBuyer)
-		authorizedRoutesBuyer.GET("/buyerorders/", h.AllBuyerOrders)
+		authorizedRoutesBuyer.GET("/buyerorders", h.AllBuyerOrders)
 	}
 	authorizedRoutesSeller := apirouter.Group("/")
 	authorizedRoutesSeller.Use(middleware.AuthorizeSeller(h.DB.FindSellerByEmail, h.DB.TokenInBlacklist))
 	{
 
 		authorizedRoutesSeller.PUT("/updatesellerprofile", h.UpdateSellerProfileHandler)
-		authorizedRoutesSeller.GET("/sellerorders/", h.AllSellerOrders)
+		authorizedRoutesSeller.GET("/sellerorders", h.AllSellerOrders)
 		authorizedRoutesSeller.GET("/seller/totalorder/", h.SellerTotalOrders)
 		authorizedRoutesSeller.GET("/getsellerprofile", h.GetSellerProfileHandler)
 		authorizedRoutesSeller.GET("/seller/total/product/sold", h.GetTotalSoldProductCount)
