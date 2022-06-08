bd45295fdeac3f05ddf7aa517ca335b2213f5430
majorchork <majorchork@gmail.com>
2022-06-08 16:14:33 +0100

    rating update

--- handlers/ratingHandler.go
+++ handlers/ratingHandler.go
@@ -47,3 +47,44 @@ func (h *Handler) SellerRating(c *gin.Context) {
 	log.Println(seller.Rating)
 	c.JSON(200, gin.H{"message": "thank you for your feedback"})
 }
+func (h *Handler) ProductRating(c *gin.Context) {
+	var ratingRequest models.RatingRequest
+	userI, exists := c.Get("user")
+	if !exists {
+		c.JSON(500, gin.H{"message": "internal server get  error"})
+	}
+	_, ok := userI.(*models.Buyer)
+	if !ok {
+		c.JSON(500, gin.H{"message": "invalid assert"})
+	}
+	err := c.BindJSON(&ratingRequest)
+	if err != nil {
+		c.JSON(404, gin.H{"message": "please fill al fields"})
+		c.Abort()
+		return
+	}
+	product, err := h.DB.FindProductById(ratingRequest.Id)
+	if err != nil {
+		c.JSON(500, gin.H{"message": "Something Went Wrong!"})
+		c.Abort()
+		return
+	}
+	product.TotalRatings += ratingRequest.Rating
+	product.NumberOfRatingsReceived += 1
+	log.Println("test1")
+
+	product.Rating = product.TotalRatings / product.NumberOfRatingsReceived
+	var updateDetail = models.UpdateRating{
+		Rating:                  product.Rating,
+		TotalRatings:            product.TotalRatings,
+		NumberOfRatingsReceived: product.NumberOfRatingsReceived,
+	}
+	err = h.DB.UpdateProductRating(product.ID, &updateDetail)
+	if err != nil {
+		c.JSON(500, gin.H{"message": "Something went wrong, please try again"})
+		c.Abort()
+		return
+	}
+	log.Println(product.Rating)
+	c.JSON(200, gin.H{"message": "thank you for your feedback"})
+}
